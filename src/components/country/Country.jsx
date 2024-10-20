15356ffbd0fe2060f61418080858ce5608e9ee4a
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-10-21 00:25:44 +0600

    population and area added

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,13 +1,17 @@
 import "./country.css";
 
 export default function Country({ country }) {
-  const { name, flags } = country;
+  const { name, flags, area,population } = country;
   console.log(country);
   return (
     <div>
       <div className="country-container">
-        <h3>Name : {name.common}</h3>
-        <img src={flags.png} alt="" />
+        <h3>Name : {name?.common}</h3>
+        <img src={flags?.png} alt="" />
+        <p>Area : <small>{area}</small></p>
+        <p>Population : {population}</p>
+      <button>Visit</button>
+      <button>Mark Visit</button>
       </div>
     </div>
   );
